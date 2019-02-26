cbd6eee3777c79fb7f0adb3d80c602a247d1c374
alemor10 <alexx1231@mail.fresnostate.edu>
2019-02-26 15:31:50 -0800

    facebook + google sign in updated
    SignUp screen refactored and more clean as well

--- src/stacknav/SignUpScreen.js
+++ src/stacknav/SignUpScreen.js
@@ -1,69 +1,31 @@
 import React, { Component } from 'react';
-import { AppRegistry, Button,StyleSheet,Text,View,Alert,NativeModules,TouchableOpacity  } from 'react-native';
+import { AppRegistry, Button,StyleSheet,Text,View,Alert,NativeModules,TouchableOpacity,  } from 'react-native';
 import firebase from 'react-native-firebase'
 //google
-import { GoogleSignin, GoogleSigninButton, statusCodes  } from 'react-native-google-signin';
+
 //facebook
 import { LoginButton,AccessToken, LoginManager } from 'react-native-fbsdk';
 // importing twitter button 
-import TwitterButton from './TwitterButton'
+import TwitterButton, { twitterLogin } from './TwitterSignIn'
+import  GoogleSign, { googleLogin } from './GoogleSignIn'
+import { facebookLogin } from './FacebookSignin';
 
 
 export default class SignUp extends Component {
-
-  componentDidMount() {
-    GoogleSignin.configure({
-      androidClientId: '106823834577-oa56hftvr714h6avalhupjon5lps3lcd.apps.googleusercontent.com', // only for android
-        forceConsentPrompt: true, // if you want to show the authorization prompt at each login
-    });
-
-    googleSignInHandler = () => {
-      GoogleSignin.hasPlayServices()
-      .then(res => {
-          GoogleSignin.signIn()
-          .then(res => {
-              console.log(res);
-          })
-          .catch(err => {
-              console.log(error.code);
-          });
-      })
-      .catch(err => {
-          console.log(err);
-      });
-  }
   
-  }
-
-  render() {
-    
+  render() {  
     return (
       <View>
-      <LoginButton
-        onLoginFinished={
-          (error, result) => {
-            if (error) {
-              console.log("login has error: " + result.error);
-            } else if (result.isCancelled) {
-              console.log("login is cancelled.");
-            } else {
-              AccessToken.getCurrentAccessToken().then(
-                (data) => {
-                  console.log(data.accessToken.toString())
-                }
-              )
-            }
-          }
-        }
-        onLogoutFinished={() => console.log("logout.")}/>
-         <TwitterButton></TwitterButton>
+        <Button title = "Sign in with Facebook"
+                onPress = { facebookLogin}></Button>
 
-         <GoogleSigninButton
-           style={{ width: 192, height: 48 }}
-           size={GoogleSigninButton.Size.Wide}
-           color={GoogleSigninButton.Color.Dark}
-           onPress={this.googleSignInHandler}
-            />
+        <Button title="Sign in with Twitter"
+                onPress = {twitterLogin}>
+               </Button>
+
+        <Button title="Sign in with Google"
+                onPress = { googleLogin } ></Button>
+        
 
         </View>
     );
