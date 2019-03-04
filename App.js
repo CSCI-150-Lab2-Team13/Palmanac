ac18a730d76c272ae44dc711cfc47d1b78a9037b
alemor10 <alexx1231@mail.fresnostate.edu>
2019-03-04 15:56:10 -0800

    restructure of project flow adding auth flow

--- App.js
+++ App.js
@@ -6,31 +6,26 @@
  * @flow
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator , createAppContainer} from 'react-navigation';
 
-import React, {Component} from "react";
-import {Platform, StyleSheet, Text, View, ActivityIndicator} from "react-native";
-import {createSwitchNavigator,
-  createAppContainer,
-  createDrawerNavigator,
-  createBottomTabNavigator,
-  createStackNavigator, StackNavigator } from 'react-navigation';
+import firebase from '@firebase/app'
+import '@firebase/auth'
 
-  import firebase from '@firebase/app';
+//importing router 
+
+import createSwitchNavigator from './src/routing/router';
 
 // import the different screens for different scenario's for StackNav
-import AuthLoading from './src/stacknav/AuthLoading'
-import SignUpScreen from './src/stacknav/SignUpScreen'
-import WelcomeScreen from './src/stacknav/WelcomeScreen'
-import HomeScreen from './src/stacknav/HomeScreen'
+import SignUpScreen from './src/mainScreens/SignUpScreen'
+import HomeScreen from './src/mainScreens/HomeScreen'
 import HardEventFormScreen from './src/forms/addHardEvent'
-//import the different screens for different scenario's for tabNav
-import Feed from './src/tabnav/Feed'
-import Profile from './src/tabnav/Profile'
-import Messages from './src/tabnav/Messages'
+
 
 //import the different screens for drawNav
 
-import Settings from './src/drawnav/SettingsScreen'
+import Settings from './src/swipeLeftScreens/SettingsScreen'
 
 /**
  * - AppSwitchNavigator
@@ -59,96 +54,25 @@ var config = {
 firebase.initializeApp(config);
 
 export default class App extends React.Component {
+
   render() {
-    return <AppContainer />;
+    return (
+      
+        <AppContainer />
+      
+    );
   }
 }
 
+// const AppStackNavigator = createStackNavigator({
+//   Login: LoginScreen,
+//   Home : HomeScreen
+// })
 
-const DashboardTabNavigator = createBottomTabNavigator({
-  Feed,
-  Profile,
-  Messages,
-}, 
-{
-  navigationOptions:({navigation})=>{
-    const {routeName} = navigation.state.routes[navigation.state.index]
-    return {
-      headerTitle: routeName
-    };
-  }
-});
+console.log(createSwitchNavigator);
 
-const DashboardStackNavigator = createStackNavigator ({
-  DashboardTabNavigator : DashboardTabNavigator
-
-  },
-  {
-    defaultNavigationOptions: ({navigation}) => {
-      return {
-        //headerLeft: <Icon name="md-menu" size={30} />
-      };
-    }
-
-  }
-);
-
-const AppDrawerNavigator = createDrawerNavigator( {
-
-  Home: {
-    screen:DashboardStackNavigator
-  },
-
-  Settings: {
-    screen: Settings
-  },
-
-});
-
-
-
-const AppSwitchNavigator = createStackNavigator ({
-  Home: {
-    screen: AppDrawerNavigator,
-  },
-  Auth: {
-    screen: AuthLoading
-  },
-  SignUp: {
-    screen:SignUpScreen,
-  },
-  Welcome: {
-    screen:WelcomeScreen,
-  },
-  HardEvent: {
-    screen:HardEventFormScreen,
-  },
-
-},
-  {initialRouteName: 'Welcome'});
-
+const AppSwitchNavigator = createSwitchNavigator;
 
 const AppContainer = createAppContainer(AppSwitchNavigator);
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
 
 
---
+++ src/routing/AuthLoading.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+    ActivityIndicator,
+    AsyncStorage,
+    StatusBar,
+    StyleSheet,
+    View, Text
+} from 'react-native';
+import firebase from '@firebase/app';
+import '@firebase/auth'
+
+export default class AuthLoadingScreen extends React.Component {
+    constructor(props) {
+        super(props);
+        this._bootstrapAsync();
+    }
+
+    // Fetch the token from storage then navigate to our appropriate place
+    _bootstrapAsync = async () => {
+        const userToken = firebase.auth().currentUser;
+        // await AsyncStorage.getItem('userToken');
+
+        console.log(userToken ? 'App' : 'Auth');
+
+        // This will switch to the App screen or Auth screen and this loading
+        // screen will be unmounted and thrown away.
+        this.props.navigation.navigate(userToken ? 'App' : 'Auth');
+
+    };
+
+    componentDidMount() {
+        firebase.auth().onAuthStateChanged((user) => {
+          if (user) {
+    
+            this.props.navigation.navigate('App');
+          } else {
+            this.props.navigation.navigate('Auth');
+          }
+        });
+      }
+    
+
+    // Render any loading content that you like here
+    render() {
+        return (
+            <View>
+                <ActivityIndicator />
+                <StatusBar barStyle="default" />
+            </View>
+        );
+    }
+}
