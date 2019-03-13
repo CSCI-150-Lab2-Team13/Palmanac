e740ddc893c3256ca9508c8ef8a97c02a80c4eb7
Sukhraj Sekhon <sukhraj5@mail.fresnostate.edu>
2019-03-12 19:00:19 -0700

    Fixed year day

--- src/forms/addHardEvent.js
+++ src/forms/addHardEvent.js
@@ -421,13 +421,10 @@ export default class HardEventFormView extends Component {
         }
         else if (freq.value == "Yearly" && interval.value){
           //var now = new Date();
-          var start = new Date(formValues.startTime.getFullYear(), 0, 0);
-          var diff = (now - start) + ((start.getTimezoneOffset() - now.getTimezoneOffset()) * 60 * 1000);
-          var oneDay = 1000 * 60 * 60 * 24;
-          var day = Math.floor(diff / oneDay);
+ 
           rule = new RRule({
             freq: RRule.YEARLY,
-            byyearday: monthVals,
+            byyearday: day,
             dtstart: formValues.startTime,
           })
          
