984f86483f24609fd0db064ca9ec6341a2105fa5
alemor10 <alexx1231@mail.fresnostate.edu>
2019-03-21 04:16:18 -0700

    messaging implementation

--- src/bottomScreens/Messages.js
+++ src/bottomScreens/Messages.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
-import { Text, View,TextInput, TouchableOpacity } from 'react-native'
+import { Text, 
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  ListView,
+  Image,
+  Button,
+  TextInput
+ } from 'react-native'
 
+import firebase from 'react-native-firebase'
 
-import firebase from '@firebase/app'
-
-import * as FirebaseAPI from '../firebase/FirebaseAPI'
+ import { GiftedChat } from 'react-native-gifted-chat'
 
 
 
@@ -13,39 +20,102 @@ export default class Messages extends Component {
 
   constructor(props) {
     super(props);
+    this.ref = firebase.firestore().collection("chat")
+
     this.state = {
-      textMessage: ''
-    }
-  }
-  
-  handleChange = key => val => {
-    this.setState({ [key]: val })
-  }
-
-  sendMessage = async () => {
-    if(this.state.textMessage.length > 0) {
-      let msgID = 
+      formCompleted: false,
+      user: {
+        _id: undefined,
+        name: undefined, 
+        avatar: undefined
+      },
+      messages: []
     }
 
-
-  }
-
-  render () {
-    return  (
-      <View style={{flexDirection: 'row'}}>
-        <TextInput
-          value = {this.state.textMessage}
-          placeholder= "Type message...."
-          onChangeText= {this.handleChange('textMessage')}
-          />
-          <TouchableOpacity onPress= {this.sendMessage}>
-            <Text> Send </Text>
-          </TouchableOpacity>
-
-      </View>
-    )
+    this.onSend = this.onSend.bind(this)
+    this.completeForm = this.completeForm.bind(this)
   }
 
 
+  componentDidMount() {
+    this.ref.orderBy("createdAt", "desc").onSnapshot(snapshot => {
+      let receivedMessages = []
 
-}  
+
+      snapshot.docs.map(doc => {
+        receivedMessages.push ({
+          _id: doc.id,
+          ...doc.data()
+        })
+      })
+
+
+      this.setState(prevState => ({
+        messages: GiftedChat.append(prevState.message, receivedMessages)
+      }))
+      })
+  }
+
+
+onSend([message]) {
+  this.ref.add(message)
+}
+
+
+completeForm() {
+  this.setState(prevState => ( {
+    formCompleted: !prevState.formCompleted
+  }))
+}
+
+
+
+render() {
+  const { formCompleted, messages , user } = this.state
+
+
+  if(formCompleted) {
+    return <GiftedChat messages={messages} onSend= {this.onSend} user={user} />
+  }
+ 
+  return (
+    <View>
+      <Text>Form</Text>
+      <TextInput
+        placeholder="id"
+        onChangeText={text =>
+          this.setState(prevState => ({
+            user: {
+              ...prevState.user,
+              _id: text
+            }
+          }))
+        }
+      />
+      <TextInput
+        placeholder="name"
+        onChangeText={text =>
+          this.setState(prevState => ({
+            user: {
+              ...prevState.user,
+              name: text
+            }
+          }))
+        }
+      />
+      <TextInput
+        placeholder="avatar"
+        onChangeText={text =>
+          this.setState(prevState => ({
+            user: {
+              ...prevState.user,
+              avatar: text
+            }
+          }))
+        }
+      />
+      <Button onPress={this.completeForm} title="Complete Form" />
+    </View>
+   )
+  }
+}
