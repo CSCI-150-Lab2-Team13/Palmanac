2c79a48bc66939b5ca2a6d7781b53615fcdbeadf
alemor10 <alexx1231@mail.fresnostate.edu>
2019-03-21 20:18:11 -0700

    update to profile,js

--- src/bottomScreens/Profile.js
+++ src/bottomScreens/Profile.js
@@ -1,50 +1,34 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
-import firebase from 'firebase';
-import firestoreAPI from '../firebase/firestoreAPI'
-import FileUploader from 'react-firebase-file-uploader';
+import { Icon } from 'native-base'
 
-
-
-
-import defaultAvatar from '../assets/default_profile.png'
 class Profile extends Component {
 
-  state = {
-    username: "",
-    avatar: "",
-    isUploading: false,
-    progress: 0,
-    avatarURL: ""
-  };
+    static navigationOptions = {
 
-  handleChangeUsername = event =>
-    this.setState({ username: event.target.value });
-  handleUploadStart = () => this.setState({ isUploading: true, progress: 0 });
-  handleProgress = progress => this.setState({ progress });
-  handleUploadError = error => {
-    this.setState({ isUploading: false });
-    console.error(error);
-  };
-  handleUploadSuccess = filename => {
-    this.setState({ avatar: filename, progress: 100, isUploading: false });
-    firebase
-      .storage()
-      .ref("images")
-      .child(filename)
-      .getDownloadURL()
-      .then(url => this.setState({ avatarURL: url }));
-  };
- 
-  render() {
-    return (
-      <View>
-      <Text> Messages </Text>
-    </View>
-    );
-  }
+        tabBarIcon: ({ tintColor }) => (
+            <Icon name="person" style={{ color: tintColor }} />
+        )
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>ProfileTab</Text>
+            </View>
+        );
+    }
 }
 
-
 export default Profile;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+});
+
+
