c8e5eee81632c549b2aba357b2c69259714bbf98
alemor10 <alexx1231@mail.fresnostate.edu>
2019-03-23 12:58:53 -0700

    react-native downgrade

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,14 +1,12 @@
 //added to address issue alemor10 had after installing twitter-sign-in through npm 
 //https://github.com/facebook/react-native/issues/21242
 
-
 const blacklist = require('metro-config/src/defaults/blacklist');
 
-// blacklist is a function that takes an array of regexes and combines
-// them with the default blacklist to return a single regex.
-
 module.exports = {
-  resolver: {
-    blacklistRE: blacklist([/dist\/.*/])
-  }
+    resolver: {
+        blacklistRE: blacklist([
+            /node_modules\\.*\\node_modules\\react-native\\.*/,
+        ])
+    },
 };
