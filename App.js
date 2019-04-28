73aed0916184d20ed125a355faeb7c3a54be2dc4
Omar Flores <OmarFlores114@mail.fresnostate.edu>
2019-04-28 15:21:33 -0700

    Implemented console notifications

--- App.js
+++ App.js
@@ -7,10 +7,11 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View } from 'react-native';
 import {createAppContainer} from "react-navigation";
-import firebase from 'react-native-firebase'
-
+import firebase from 'react-native-firebase';
+import type { Notification, NotificationOpen } from 'react-native-firebase';
+import cloudMessaging from './src/firebase/CloudMessaging';
 
 //importing router 
 
@@ -44,6 +45,64 @@ export default class App extends React.Component {
     super(props);
     
    
+}
+
+async componentDidMount() {
+  const notificationOpen: NotificationOpen = await firebase.notifications().getInitialNotification();
+  if (notificationOpen) {
+      const action = notificationOpen.action;
+      const notification: Notification = notificationOpen.notification;
+      var seen = [];
+      alert(JSON.stringify(notification.data, function(key, val) {
+          if (val != null && typeof val == "object") {
+              if (seen.indexOf(val) >= 0) {
+                  return;
+              }
+              seen.push(val);
+          }
+          return val;
+      }));
+  } 
+  const channel = new firebase.notifications.Android.Channel('test-channel', 'Test Channel', firebase.notifications.Android.Importance.Max)
+          .setDescription('My apps test channel');
+// Create the channel
+  firebase.notifications().android.createChannel(channel);
+  this.notificationDisplayedListener = firebase.notifications().onNotificationDisplayed((notification: Notification) => {
+      // Process your notification as required
+      // ANDROID: Remote notifications do not contain the channel ID. You will have to specify this manually if you'd like to re-display the notification.
+  });
+  this.notificationListener = firebase.notifications().onNotification((notification: Notification) => {
+      // Process your notification as required
+      notification
+          .android.setChannelId('test-channel')
+          .android.setSmallIcon('ic_launcher');
+      firebase.notifications()
+          .displayNotification(notification);
+      
+  });
+  this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen: NotificationOpen) => {
+      // Get the action triggered by the notification being opened
+      const action = notificationOpen.action;
+      // Get information about the notification that was opened
+      const notification: Notification = notificationOpen.notification;
+      var seen = [];
+      alert(JSON.stringify(notification.data, function(key, val) {
+          if (val != null && typeof val == "object") {
+              if (seen.indexOf(val) >= 0) {
+                  return;
+              }
+              seen.push(val);
+          }
+          return val;
+      }));
+      firebase.notifications().removeDeliveredNotification(notification.notificationId);
+      
+  });
+}
+componentWillUnmount() {
+  this.notificationDisplayedListener();
+  this.notificationListener();
+  this.notificationOpenedListener();
 }
   render() {
     return (
